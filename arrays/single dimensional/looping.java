91eb9aebe4bd9ad5df204852311695d35b9490a6
lwena27 <lwena027@gmail.com>
2024-12-16 13:24:33 +0300

    adding more element in an array will result to error

---
+++ arrays/single dimensional/looping.java
@@ -0,0 +1,11 @@
+public class looping {
+    public static void main(String[] args) {
+        String a[]={"James","Juma","CLeo","Neema"};
+        System.out.println(a[1]);
+        int i;
+        for(i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+    
+}
